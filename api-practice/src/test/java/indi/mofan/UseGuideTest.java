940a0a95671828bf694fa4fed6f6e3250aa37f7b
mofan <cy.mofan@foxmail.com>
2024-01-20 16:09:16 +0800

    1. $N 2. format code block 3. constructor 4. ParameterSpec

--- api-practice/src/test/java/indi/mofan/UseGuideTest.java
+++ api-practice/src/test/java/indi/mofan/UseGuideTest.java
@@ -1,12 +1,15 @@
 package indi.mofan;
 
 import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.CodeBlock;
 import com.squareup.javapoet.JavaFile;
 import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.ParameterSpec;
 import com.squareup.javapoet.ParameterizedTypeName;
 import com.squareup.javapoet.TypeSpec;
 import indi.mofan.constant.TestConstants;
 import lombok.SneakyThrows;
+import org.assertj.core.api.WithAssertions;
 import org.junit.jupiter.api.Test;
 
 import javax.lang.model.element.Modifier;
@@ -14,12 +17,15 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
+
+import static indi.mofan.constant.TestConstants.DEFAULT_PACKAGE_NAME;
 
 /**
  * @author mofan
  * @date 2024/1/15 22:55
  */
-public class UseGuideTest {
+public class UseGuideTest implements WithAssertions {
 
     private static final String HELLO_WORLD_CLASS_NAME = "HelloWorld";
     private static final File TARGET_FILE = new File(TestConstants.DEFAULT_GENERATED_PATH);
@@ -39,7 +45,7 @@ public class UseGuideTest {
                 .addMethod(main)
                 .build();
 
-        JavaFile javaFile = JavaFile.builder(TestConstants.DEFAULT_PACKAGE_NAME, helloWorld).build();
+        JavaFile javaFile = JavaFile.builder(DEFAULT_PACKAGE_NAME, helloWorld).build();
 
         javaFile.writeTo(TARGET_FILE);
     }
@@ -107,7 +113,7 @@ public class UseGuideTest {
                 .build();
         methodSpecs.add(main4);
 
-        JavaFile javaFile = JavaFile.builder(TestConstants.DEFAULT_PACKAGE_NAME, typeBuilder.addMethods(methodSpecs).build())
+        JavaFile javaFile = JavaFile.builder(DEFAULT_PACKAGE_NAME, typeBuilder.addMethods(methodSpecs).build())
                 .build();
         javaFile.writeTo(TARGET_FILE);
     }
@@ -129,7 +135,7 @@ public class UseGuideTest {
                 .addMethod(main)
                 .build();
 
-        JavaFile javaFile = JavaFile.builder(TestConstants.DEFAULT_PACKAGE_NAME, $L).build();
+        JavaFile javaFile = JavaFile.builder(DEFAULT_PACKAGE_NAME, $L).build();
         javaFile.writeTo(TARGET_FILE);
     }
 
@@ -148,7 +154,7 @@ public class UseGuideTest {
                 .addMethods(List.of(whatsMyName("slimShady"), whatsMyName("eminem"), whatsMyName("marshallMathers")))
                 .build();
 
-        JavaFile javaFile = JavaFile.builder(TestConstants.DEFAULT_PACKAGE_NAME, $S)
+        JavaFile javaFile = JavaFile.builder(DEFAULT_PACKAGE_NAME, $S)
                 .build();
 
         javaFile.writeTo(TARGET_FILE);
@@ -167,7 +173,7 @@ public class UseGuideTest {
         methodSpecs.add(methodSpec);
 
         // 引用生成的类
-        ClassName helloWorld = ClassName.get(TestConstants.DEFAULT_PACKAGE_NAME, HELLO_WORLD_CLASS_NAME);
+        ClassName helloWorld = ClassName.get(DEFAULT_PACKAGE_NAME, HELLO_WORLD_CLASS_NAME);
         MethodSpec tomorrow = MethodSpec.methodBuilder("tomorrow")
                 .returns(helloWorld)
                 .addStatement("return new $T()", helloWorld)
@@ -191,7 +197,107 @@ public class UseGuideTest {
                 .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
                 .addMethods(methodSpecs)
                 .build();
-        JavaFile javaFile = JavaFile.builder(TestConstants.DEFAULT_PACKAGE_NAME, $T).build();
+        JavaFile javaFile = JavaFile.builder(DEFAULT_PACKAGE_NAME, $T).build();
+        javaFile.writeTo(TARGET_FILE);
+    }
+
+    @Test
+    @SneakyThrows
+    public void test$N() {
+        MethodSpec hexDigit = MethodSpec.methodBuilder("hexDigit")
+                .returns(char.class)
+                .addParameter(int.class, "i")
+                .addStatement("return (char) (i < 10 ? i + '0' : i - 10 + 'a')")
+                .build();
+
+        MethodSpec byteToHex = MethodSpec.methodBuilder("byteToHex")
+                .addParameter(int.class, "b")
+                .returns(String.class)
+                .addStatement("char[] result = new char[2]")
+                .addStatement("result[0] = $N((b >>> 4) & 0xf)", hexDigit)
+                .addStatement("result[1] = $L(b & 0xf)", "hexDigit")
+                .addStatement("return new String(result)")
+                .build();
+
+        TypeSpec typeSpec = TypeSpec.classBuilder("$N")
+                .addMethods(List.of(byteToHex, hexDigit))
+                .build();
+
+        JavaFile javaFile = JavaFile.builder(DEFAULT_PACKAGE_NAME, typeSpec).build();
+        javaFile.writeTo(TARGET_FILE);
+    }
+
+    /**
+     * 每种占位符风格在一个 CodeBlock 上只能使用一种。
+     */
+    @Test
+    public void testFormatCodeBlock() {
+        CodeBlock block = CodeBlock.builder().add("I ate $L $L", 3, "tacos").build();
+        assertThat(block).asString().isEqualTo("I ate 3 tacos");
+
+        // 使用序号指定使用哪个位置的参数
+        block = CodeBlock.of("I ate $2L $1L", "tacos", 3);
+        assertThat(block).asString().isEqualTo("I ate 3 tacos");
+
+        // 命名参数必须以小写字母开头
+        var map = Map.of("food", "tacos", "count", 3);
+        // 要调用 `addNamed()` 方法
+        block = CodeBlock.builder().addNamed("I ate $count:L $food:L", map).build();
+        assertThat(block).asString().isEqualTo("I ate 3 tacos");
+    }
+
+    @Test
+    @SneakyThrows
+    public void testAbstractMethods() {
+        MethodSpec methodSpec = MethodSpec.methodBuilder("flux")
+                .addModifiers(Modifier.ABSTRACT, Modifier.PROTECTED)
+                .build();
+
+        TypeSpec typeSpec = TypeSpec.classBuilder("BaseClass")
+                .addModifiers(Modifier.PUBLIC, Modifier.ABSTRACT)
+                .addMethod(methodSpec)
+                .build();
+
+        JavaFile javaFile = JavaFile.builder(DEFAULT_PACKAGE_NAME, typeSpec).build();
+        javaFile.writeTo(TARGET_FILE);
+    }
+
+    @Test
+    @SneakyThrows
+    public void testConstructor() {
+        MethodSpec constructor = MethodSpec.constructorBuilder()
+                .addModifiers(Modifier.PUBLIC)
+                .addParameter(String.class, "name")
+                .addStatement("this.$L = $L", "name", "name")
+                .build();
+
+        TypeSpec person = TypeSpec.classBuilder("Person")
+                .addModifiers(Modifier.PUBLIC)
+                .addField(String.class, "name", Modifier.PRIVATE, Modifier.FINAL)
+                .addMethod(constructor)
+                .build();
+
+        JavaFile javaFile = JavaFile.builder(DEFAULT_PACKAGE_NAME, person).build();
+        javaFile.writeTo(TARGET_FILE);
+    }
+
+    @Test
+    @SneakyThrows
+    public void testParameters() {
+        ParameterSpec android = ParameterSpec.builder(String.class, "android")
+                .addModifiers(Modifier.FINAL)
+                .build();
+
+        MethodSpec methodSpec = MethodSpec.methodBuilder("welcomeOverlords")
+                .addParameter(android)
+                .addParameter(String.class, "robot", Modifier.FINAL)
+                .build();
+
+        TypeSpec typeSpec = TypeSpec.classBuilder("Parameter")
+                .addMethod(methodSpec)
+                .build();
+
+        JavaFile javaFile = JavaFile.builder(DEFAULT_PACKAGE_NAME, typeSpec).build();
         javaFile.writeTo(TARGET_FILE);
     }
 }
